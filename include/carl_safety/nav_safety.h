70ccdfb1a57332fbe3eaf30524784ff0eb3791a1
Russell Toris <rctoris@wpi.edu>
2014-12-04 07:59:35 -0500

    Rebase from 'release/indigo/carl_safety'

--- include/carl_safety/nav_safety.h
+++ include/carl_safety/nav_safety.h
@@ -15,10 +15,14 @@
 
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
+#include <actionlib/server/simple_action_server.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Twist.h>
 #include <move_base_msgs/MoveBaseAction.h>
+#include <sensor_msgs/JointState.h>
 #include <sensor_msgs/Joy.h>
+#include <wpi_jaco_msgs/GetAngularPosition.h>
+#include <wpi_jaco_msgs/HomeArmAction.h>
 
 //controller types
 #define ANALOG 0 //analog triggers
@@ -46,12 +50,6 @@ public:
   NavSafety();
 
   /**
-   * \brief getter for stopped
-   * @return true if safe nav commands should be stopped
-   */
-  bool isStopped();
-
-  /**
    * \brief cancels all nav goals
    */
   void cancelNavGoals();
@@ -75,6 +73,8 @@ private:
    */
   void poseCallback(const geometry_msgs::Pose::ConstPtr& msg);
 
+  void safeMoveCallback(const move_base_msgs::MoveBaseGoalConstPtr &goal);
+
   ros::NodeHandle node; /*!< a handle for this ROS node */
 
   ros::Publisher baseCommandPublisher; /*!< actual base command publisher */
@@ -82,13 +82,21 @@ private:
   ros::Subscriber joySubscriber; /*!< subscriber for joystick input */
   ros::Subscriber robotPoseSubscriber; /*!< subscriber for the robot base pose */
 
+  ros::ServiceClient jacoPosClient;
+
   actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> acMoveBase;
+  actionlib::SimpleActionClient<wpi_jaco_msgs::HomeArmAction> acHome;
+
+  actionlib::SimpleActionServer<move_base_msgs::MoveBaseAction> asSafeMove;
 
   int controllerType;
   bool stopped; /*!< true if safe nav commands should be stopped */
   float x;
   float y;
   float theta;
+  std::vector<float> retractPos; //jaco arm retracted joint positions
+
+  bool use_teleop_safety; /*!< launch param to determine which node to publish to */
 };
 
 #endif
